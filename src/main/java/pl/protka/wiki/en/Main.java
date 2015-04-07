0aa835e5e6b2f7078106f0b66d55a061f97d5e6a
mgruszka <mgruszek91@gmail.com>
2015-04-08 01:54:28 +0200

    initial commit

---
+++ src/main/java/pl/protka/wiki/en/Main.java
@@ -0,0 +1,16 @@
+package pl.protka.wiki.en;
+
+/**
+ *  Created by gruszek on 08.04.15.
+ */
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        System.out.println("Hello");
+
+    }
+
+
+}
